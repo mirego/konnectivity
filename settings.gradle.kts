7b8230ed0c43dfafffa0fabf91ab4a9195106139
Marc-Antoine Fortier <mafortier@mirego.com>
2022-12-03 14:55:06 -0500

    Make library publishable to Mirego's Maven (#2)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,17 @@
 pluginManagement {
-    repositories {
-        google()
-        gradlePluginPortal()
-        mavenCentral()
+    resolutionStrategy {
+        repositories {
+            google()
+            gradlePluginPortal()
+            mavenCentral()
+            maven("https://s3.amazonaws.com/mirego-maven/public")
+        }
+
+        eachPlugin {
+            when (val namespace = requested.id.namespace) {
+                "mirego" -> useModule("$namespace:${requested.id.name}-plugin:${requested.version}")
+            }
+        }
     }
 }
 
