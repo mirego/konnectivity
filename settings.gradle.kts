b40f3b8cd47f19cd67fa1787aa084e44ea9e8571
Marc-Antoine Fortier <mafortier@mirego.com>
2023-03-27 11:17:08 -0400

    Update to Kotlin 1.8.10

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,17 +1,26 @@
-pluginManagement {
-    resolutionStrategy {
-        repositories {
-            google()
-            gradlePluginPortal()
-            mavenCentral()
-            maven("https://s3.amazonaws.com/mirego-maven/public")
-        }
+@file:Suppress("UnstableApiUsage")
 
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+        maven("https://s3.amazonaws.com/mirego-maven/public")
+    }
+
+    resolutionStrategy {
         eachPlugin {
             when (val namespace = requested.id.namespace) {
                 "mirego" -> useModule("$namespace:${requested.id.name}-plugin:${requested.version}")
             }
         }
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
     }
 }
 
