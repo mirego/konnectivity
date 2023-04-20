f14f8bb3be9487db2a6967902bdf6ca296e32e31
Marc-Antoine Fortier <mafortier@mirego.com>
2023-04-20 17:48:55 -0400

    Expose KonnectivityInitializer to allow lib users to order their initialization process

--- konnectivity/src/androidMain/kotlin/com/mirego/konnectivity/Konnectivity.kt
+++ konnectivity/src/androidMain/kotlin/com/mirego/konnectivity/Konnectivity.kt
@@ -1,17 +1,3 @@
 package com.mirego.konnectivity
 
-import android.content.Context
-import androidx.startup.Initializer
-
-private lateinit var appContext: Context
-
 actual fun Konnectivity(): Konnectivity = AndroidKonnectivity(appContext)
-
-internal class KonnectivityInitializer : Initializer<Context> {
-
-    override fun create(context: Context): Context = context.applicationContext.also {
-        appContext = it
-    }
-
-    override fun dependencies(): List<Class<out Initializer<*>>> = emptyList()
-}
